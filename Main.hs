296b0a4899a0dbe2617cd7ee775d966ac3ba6e6e
shika-blyat <abdelzighel@gmail.com>
2020-02-13 03:18:52 +0100

    Add counter for typevariable in infer signature

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Prelude hiding (lookup)
 
-import Data.Map (Map, lookup, empty)
+import Data.Map (Map, lookup, empty, singleton)
 
 data Expr = EVar String | ELit Literal | ECall Expr Expr | EFun String Expr
 
@@ -13,18 +13,16 @@ data Type = TNamed String | TVar String | TFun Type Type deriving Show
 type Context = Map String Type
 type Subst = Map String Type
 
-infer :: Expr -> Context -> Either (Type, Subst) String
+infer :: Expr -> Context -> Int -> Either (Type, Subst) String
 
-infer (ELit lit) _ = Left (TNamed $ show lit, empty)
+infer (ELit lit) _ _ = Left (TNamed $ show lit, empty)
 
-infer (EVar name) ctx = case Data.Map.lookup name ctx of
+infer (EVar name) ctx _ = case Data.Map.lookup name ctx of
     Just val -> Left (val, empty)
     Nothing -> Right ("Use of undeclared variable" ++ show name)
 
---infer (ECall from to) ctx = case Data.Map.lookup name ctx of
-    -- Just val -> Left val
-    -- Nothing -> Right ("Use of undeclared variable" ++ show name)
+--infer (ECall from to) ctx counter =
 
 main :: IO ()
 
-main = print $ infer (ELit (Number 15)) empty
+main = print $ infer (ELit (Number 15)) empty 0
